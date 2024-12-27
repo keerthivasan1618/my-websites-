6523ca41d4819ef0be8d27d9b17c5694467cfecf
keerthivasan1618 <kv1622008@gmail.com>
2024-12-27 23:59:14 +0530

    Add files via upload

---
+++ web.py
@@ -0,0 +1,41 @@
+
+body
+{
+    font-family:arial,sans-serif:
+    margin:0;
+    padding:0;
+    background-color:pink;
+    color:blue
+    }
+header
+{
+    background-color:yellow;
+    color:white;
+    padding:1 em 0;
+    text-align:center;
+    }
+header nav a
+{
+    color:white;
+    margin:0 15 px;
+    text-decoration:none;
+    }
+header nav a:hover
+{
+    text-decoration:underline;
+    }
+section
+{
+    margin:20 px;
+    padding:20 px;
+    background-color:blue;
+    border-radius:5 px;
+    box-shadow:0 0 10 px rgba(0,0,0,0.1);
+    }
+footer
+{
+    text-align:center;
+    padding:10 px 0;
+    background-color:blue;
+    color:white;
+    }
